a4d7411f7ab4e1b7f5d78edfe8a08acf364498d9
Sardor Isakov <sisardor@gmail.com>
2015-02-10 17:27:47 -0800

    update

---
+++ js/App.js
@@ -0,0 +1,29 @@
+// App.js
+var app = (function() {
+ 
+    var api = {
+        views: {},
+        models: {},
+        collections: {},
+        content: null,
+        router: null,
+        todos: null,
+        init: function() {
+            this.content = $("#content");
+        },
+        changeContent: function(el) {
+            this.content.empty().append(el);
+            return this;
+        },
+        title: function(str) {
+            $("h1").text(str);
+            return this;
+        }
+    };
+    var ViewsFactory = {};
+    var Router = Backbone.Router.extend({});
+    api.router = new Router();
+ 
+    return api;
+ 
+})();
